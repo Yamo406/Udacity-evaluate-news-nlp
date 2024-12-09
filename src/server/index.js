abd6b0d579a4c7a27899bb0045665fbbc5d7a24c
Yamo406 <93231355+Yamo406@users.noreply.github.com.>
2024-12-09 20:41:14 +0200

    init the project, set up the API KEY in the dotenv file, configure webpack in both dev and prod modes

--- src/server/index.js
+++ src/server/index.js
@@ -1,33 +1,38 @@
-var path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static("dist"));
+
 console.log(__dirname);
 
 // Variables for url and api key
+const meaningcloudConfigs = {
+  url: `${API_URL}`,
+  key: `${process.env.API_KEY}`,
+};
 
+const API_URL = `https://api.meaningcloud.com/sentiment-2.1`;
 
-app.get('/', function (req, res) {
-    res.send("This is the server API page, you may access its services via the client app.");
+app.get("/", function (req, res) {
+  // res.send("This is the server API page, you may access its services via the client app.");
+  res.sendFile("dist/index.html");
 });
-
 
 // POST Route
 
-
-
 // Designates what port the app will listen to for incoming requests
-app.listen(8000, function () {
-    console.log('Example app listening on port 8000!');
+const PORT = 9000;
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
 });
-
-
