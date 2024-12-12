52575b9b0cd2182ef8f86d8e476782ed9d19283a
Yamo406 <93231355+Yamo406@users.noreply.github.com.>
2024-12-12 04:42:04 +0200

    add styling, make the page stylish

--- src/client/index.js
+++ src/client/index.js
@@ -30,12 +30,17 @@ async function updateResults() {
         const resultsFragment = new DocumentFragment();
 
         const subjectivity = document.createElement("div");
-        const polarity = document.createElement("div");
-        const text = document.createElement("div");
-
         subjectivity.textContent = `Subjectivity: ${apiResponse.subjectivity}`;
+        subjectivity.classList.add("result");
+
+        const polarity = document.createElement("div");
         polarity.textContent = `Polarity: ${apiResponse.polarity}`;
+        polarity.classList.add("result");
+
+        const text = document.createElement("div");
         text.textContent = `Text: ${apiResponse.text}`;
+        text.classList.add("result");
+
 
         resultsFragment.appendChild(subjectivity);
         resultsFragment.appendChild(polarity);
